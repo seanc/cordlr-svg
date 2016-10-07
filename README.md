be58f45bb0ae5b690528c4760b6fa1b2c1fb025b
Sean Wilson <hello@imsean.me>
2016-10-07 00:17:43 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# cordlr-svg [![NPM version](https://badge.fury.io/js/cordlr-svg.svg)](https://npmjs.org/package/cordlr-svg) [![Build Status](https://travis-ci.org/seanc/cordlr-svg.svg?branch=master)](https://travis-ci.org/seanc/cordlr-svg)
+
+> Cordlr svg-to-png plugin
+
+## Installation
+
+```sh
+$ cordlr install cordlr-svg
+```
+
+Then add it to your config
+
+```js
+{
+  "plugins": [
+    "cordlr-svg"
+  ],
+  "svg": {
+    "max": 6 //How many images can be converted at once
+  }
+}
+```
+
+## Usage
+`svg <...urls>`
+
+## License
+
+MIT © [Sean Wilson](https://imsean.me)
---
+++ index.js
@@ -0,0 +1,47 @@
+const svgToPng = require('svg2png');
+const path = require('path');
+const request = require('request');
+const validUrl = require('valid-url').isUri;
+
+function svg(bot, config) {
+  config = config[svg.name] || {}
+  const max = config.max === 'Infinity' ? Infinity : config.max || 6;
+
+  return function run(message, args) {
+    if (!args.length) return message.reply('You need to provide at least one url');
+    const urls = args.filter(url => validUrl(url));
+
+    if (urls.length > max) {
+      return message.reply(`You can only convert ${max} images at a time`);
+    }
+
+    urls.forEach(url => {
+      if (['.png', '.jpg', '.jpeg'].indexOf(path.extname(url)) > -1) {
+        return message.reply(`The image \`${path.basename(url)}\` has already been converted`);
+      }
+
+      if (path.extname(url) !== '.svg') {
+        return message.reply('That image is not in svg format');
+      }
+
+      request(url, (err, res, body) => {
+        if (err) {
+          message.reply('An error occured while trying to convert the image to png.')
+          return console.log(err);
+        }
+
+        svgToPng(new Buffer(body.trim()))
+        .then(buffer => message.channel.sendFile(buffer))
+        .catch(err => {
+          console.log(err);
+          message.reply('An error occured while trying to convert the image to png.');
+        });
+      });
+    });
+  }
+}
+
+svg.command = 'svg';
+svg.usage = 'svg <...urls>';
+
+module.exports = svg;
